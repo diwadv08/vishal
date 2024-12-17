4c0bff14559d432244a574b5dd94545e9d890a55
Diwakar <you@example.com>
2024-12-17 18:54:27 +0530

    conditions

---
+++ js/if_else.js
@@ -0,0 +1,56 @@
+// var a=21;
+// if(a==121){
+//     alert(a)
+// }
+// else{
+//     alert('Error!')
+// }
+
+// var a=50;
+// if(a==50){
+//     alert(10+' true')
+// }
+// if(a==50){
+//     alert(10+" false")
+// }
+
+// if(a==50){
+//     alert(10+' true')
+// }
+// if(a==50){
+//     alert(10+" false")
+// }
+
+// if(a==500){
+//     alert(10+' true')
+// }
+// else {
+//     alert(10+" false")
+// }
+
+
+// var a="334";
+// if(isFinite(a)){
+//     if(a%2==0){
+//         alert(a+' is even number')
+//     }
+//     else if(a%2!=0){
+//         alert(a+' is odd number')
+//     }
+// }
+// else{
+//     alert(a+' is not a number')
+// }
+
+
+// var a='Admin';
+// if(a=='Admin'){
+//     window.open('Welcome.html')
+// }
+// else{
+//     location.href='index.html';
+// }
+
+
+//Task 5,10,15,20
+//window.open,location.href
