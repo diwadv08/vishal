2ce8e721ec0e95f3c498edbac3c74b0c21896827
Diwakar <you@example.com>
2025-01-02 18:58:22 +0530

    02-01

---
+++ js/for.js
@@ -0,0 +1,13 @@
+// for(let i=1;i<=3;i++){
+//     document.write(i);
+// }
+ 
+// for(let i=3;i>=1;i--){
+//     document.write(i);
+// }
+ 
+for(let i=2;i<=10;i+=2){
+    document.write(i+"<br>");
+}
+ 
+
---
+++ js/switch.js
@@ -0,0 +1,37 @@
+// alert(new Date())
+// alert(new Date().getFullYear())
+// alert(new Date().getDate())
+// alert(new Date().getMonth())
+// alert(new Date().getDay())
+
+
+// var day=new Date().getDay();  //4
+// var date=new Date().getDate();  //2
+// var day_array=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+// switch(day){
+//     case 0:
+//         alert(date+"-"+day_array[0])
+//         break;
+//     case 1:
+//         alert(date+"-"+day_array[1])
+//         break;
+//     case 2:
+//         alert(date+"-"+day_array[2])
+//         break;
+//     case 3:
+//         alert(date+"-"+day_array[3])
+//         break;
+//     case 4:
+//         alert(date+"-"+day_array[4])
+//         break;
+//     case 5:
+//         alert(date+"-"+day_array[5])
+//         break;
+//     case 6:
+//         alert(date+"-"+day_array[6])
+//         break;
+//     default:
+//         alert("Not a day")
+//         break;
+// }
+
