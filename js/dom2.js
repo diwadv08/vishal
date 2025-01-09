d1eacb357db6dd92809111423c965ccad048fcea
Diwakar <you@example.com>
2025-01-09 18:56:26 +0530

    09-01

---
+++ js/dom2.js
@@ -0,0 +1,32 @@
+// function Mode(e){
+//     if(e.innerHTML=='Light'){
+//         e.innerHTML='Dark';
+//         document.body.style.background='white';
+//     }
+//     else if(e.innerHTML=='Dark'){
+//         e.innerHTML='Light';
+//         document.body.style.background='black';
+//     }
+// }
+
+
+// function Change(e){
+//     var pwd=document.querySelector("#pwd")
+//     if(e.className=='fa fa-eye-slash'){
+//         pwd.type='password';
+//         e.className='fa fa-eye';
+//     }
+//     else if(e.className=='fa fa-eye'){
+//         pwd.type='text';
+//         e.className='fa fa-eye-slash';
+//     }
+// }
+
+function SubmitFn(){
+    let name=document.querySelector("#name").value;
+    let pwd=document.querySelector("#pwd").value;
+    if(name!="admin" || pwd!='123'){
+        alert("Invalid Login")
+        return false;
+    }
+}
