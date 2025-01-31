a57cf62386089d9a359d3105a197264a46745457
Diwakar <you@example.com>
2025-01-31 18:17:42 +0530

    30-01

--- filter/script.js
+++ filter/script.js
@@ -1,5 +1,5 @@
 let table_datas=document.querySelector(".table_datas");
-
+let input=document.querySelector("input");
 
 const table_data=[
     {
@@ -26,22 +26,69 @@ const table_data=[
         mobile:'98756767',
         email:'Varun@gmail.com'
     },
+    {
+        id:'5',
+        name:'Ajay',
+        mobile:'098765',
+        email:'ajay@gmail.com'
+    },
+    {
+        id:'6',
+        name:'Hari',
+        mobile:'987654387',
+        email:'harish@gmail.com'
+    },
 ];
-let table_records=`<table width='100%'><tr align='left'>`;
-let thead=Object.keys(table_data[0]);
-thead.map((e)=>{
-    table_records+=`<th>${e.toUpperCase()}</th>`;
-})
-table_records+=`</tr>`
-table_data.map((e)=>{
-    table_records+=`<tr>
-            <td>${e.id}</td>
-            <td>${e.name}</td>
-            <td>${e.mobile}</td>
-            <td>${e.email}</td>
-        </tr>`
-})
+
+function DefaultTable(){
+    let table_records=`<table width='100%'><tr align='left'>`;
+    let thead=Object.keys(table_data[0]);
+    thead.map((e)=>{
+        table_records+=`<th>${e.toUpperCase()}</th>`;
+    })
+    table_records+=`</tr>`
+    table_data.map((e)=>{
+        table_records+=`<tr>
+                <td>${e.id}</td>
+                <td>${e.name}</td>
+                <td>${e.mobile}</td>
+                <td>${e.email}</td>
+            </tr>`
+    })
+    table_datas.innerHTML=table_records;
+}
+
+const FilterTable=(e)=>{
+    let myInput=e.target.value;
+    let table_records=`<table width='100%'><tr align='left'>`;
+    let thead=Object.keys(table_data[0]);
+    thead.map((e)=>{
+        table_records+=`<th>${e.toUpperCase()}</th>`;
+    })
+    let table_result=table_data.filter((e)=>{
+        if(e.name.toLowerCase().startsWith(myInput.toLowerCase())){
+            return e;
+        }
+    }) 
+
+    if(table_result.length>0){
+        table_result.map((e)=>{
+            table_records+=`<tr>
+                    <td>${e.id}</td>
+                    <td>${e.name}</td>
+                    <td>${e.mobile}</td>
+                    <td>${e.email}</td>
+                </tr>`
+        })  
+    
+        table_datas.innerHTML=table_records;
+    }
+    else{
+        table_datas.innerHTML=`<center><p>No results found <b><u>"${myInput}"</u></b>
+   </p></center>`;
+    }
+}
+input.addEventListener('keyup',FilterTable);
 
 
-table_datas.innerHTML=table_records;
-
+DefaultTable()
