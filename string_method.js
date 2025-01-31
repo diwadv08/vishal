0f6b48fa35d274c4536c61158d3ceea2f5244caf
Diwakar <you@example.com>
2025-01-31 19:01:22 +0530

    31-01

---
+++ string_method.js
@@ -0,0 +1,25 @@
+var arr=[1,2,3,4];
+var a="Helelo     World";
+
+// console.log(a=a.replaceAll(" ","^"));
+// console.log(a=a.split('^^^^^'));
+// console.log(a=a.join(' '));
+
+// alert(Math.PI)
+let num=Math.random()*1000000;
+alert(num.toFixed())
+alert(num.Math.floor())
+
+
+// document.write(`<h1>${a.replace('World','-')}`)  //H-llo World
+// document.write(`<h1>${a.replace('World','-')}`)  //H-llo World
+// document.write(`<h1>${a.replaceAll('e','-')}`)  //H-llo World
+
+// console.log(a.split(''));
+// console.log(arr.toString());   //[1,2,3,4]="1,2,3,4"
+// console.log(arr);   //[1,2,3,4]="1,2,3,4"
+// alert('hi'+a.trimStart()+'hi')
+// alert('hi'+a.trimEnd()+'hi')
+// alert('hi'+a.trim()+'hi')
+
+// console.log(arr.join('*'))
